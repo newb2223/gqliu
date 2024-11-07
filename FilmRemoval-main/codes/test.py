75bb780056854f0a1b5200abcfb1e83937d134bf
newb2223 <159115337+newb2223@users.noreply.github.com>
2024-11-07 14:15:32 +0800

    test.py

--- FilmRemoval-main/codes/test.py
+++ FilmRemoval-main/codes/test.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 #### options
 parser = argparse.ArgumentParser()
 parser.add_argument('-opt', type=str, required=False, help='Path to options YMAL file.',
-                    default="D:/FilmRemoval-main/codes/options/test/test.yml")
+                    default="/openbayes/home/gqliu/FilmRemoval-main/codes/options/test/test.yml")
 opt = option.parse(parser.parse_args().opt, is_train=False)
 opt = option.dict_to_nonedict(opt)
 
